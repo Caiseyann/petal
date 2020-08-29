334383afe68f224baba2a05a49dd3a85f3c4f13c
caiseyann <caiseyann4@gmail.com>
2020-08-29 13:41:22 +0300

    new changes on app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './homepage/homepage.component';
+import { LandpageComponent } from './landpage/landpage.component';
+import {RoutingModule} from './routing/routing.module';
+import { GitserviceService } from './gitservice.service';
+import { from } from 'rxjs';
+import { HighlightDirective } from './highlight.directive';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    LandpageComponent,
+    HighlightDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [GitserviceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
